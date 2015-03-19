518bb1ec9d0cda3919383d3630e2ae9c02eddba5
Grimmnyr <christopherecato@gmail.com>
2015-03-19 14:33:05 -0500

    Continuing Changes
    Putting PD1 on hold and starting 2.0 using classes instead of a thousand
    if statements.

---
+++ Pokemon2.0.py
@@ -0,0 +1,43 @@
+#take two of pokemon game, going to define pokemon as a class and have the battle as a function of that class
+
+import random     
+
+Vinewhip= random.randint(10,15)
+Razorleaf = random.randint(15,25)
+Solarbeam = random.randint(20,30)
+Cut = random.randint(10,15)
+Flamethrower = random.randint(20,25)
+Slam = random.randint(10,20)
+Thunder = random.randint(25,30)
+Thunderbolt = random.randint(15,20)
+Tackle = random.randint(10,15)
+Bite = random.randint(20,30)
+Hyperbeam = random.randint(25,35)
+Slash = random.randint(15,20)
+Watercanon = random.randint(20,25)
+Squirt= random.randint(15,20)
+Hydropump = random.randint(25,35)
+Ember = random.randint(10,15)
+Firekick = random.randint(15,20)
+Flametempest = random.randint(20,25)
+class Pokemon(object):
+    #the pokemon
+    def __init__(self, name, hp, type, attack1, attack2, attack3, attack4):
+        self.name = name
+        self.hp = 100
+        self.type = type
+        self.attack1 = attack1
+        self.attack2 = attack2
+        self.attack3 = attack3
+        self.attack4 = attack4
+        
+Absol = Pokemon('Absol', 100, 'Dark', Bite, Hyperbeam, Tackle, Slash)
+
+Charizard = Pokemon('Charizard', 100, 'Fire/Flying', Flamethrower, Ember, Firekick, Flametempest)
+
+Blastoise = Pokemon('Blastoise', 100,'Water',Watercanon, Squirt, Tackle,Hydropump)
+
+Venasaur = Pokemon('Venasaur', 100, 'Grass', Vinewhip, Razorleaf, Solarbeam, Cut)
+
+Pikachu = Pokemon('Pikachu', 100, 'Electric', Slam, Thunder, Thunderbolt, Tackle)
+
--- PokemonDuel1.py
+++ PokemonDuel1.py
@@ -1,22 +1,24 @@
 import random
 
 #test pokemon like game, will add gui later
-
-pokemon1 = 100
-enemy = 100
+Absol = Absol
+Venasaur = Venasaur
+Pikachu = Pikachu
+Charizard = Charizard
+Blastoise = Blastoise
 #Let's user choose the pokemon to duel with
 print "Choose your Pokemon!"
 Selection = raw_input('Absol Charizard Blastoise Venasaur Pikachu: ')
-if Selection == Absol:
+if Selection == 'Absol':
     pokemon1 = Absol
 else:
-    if Selection == Charizard:
+    if Selection == 'Charizard':
         pokemon1 = Charizard
-    elif Selection == Blastoise:
+    elif Selection == 'Blastoise':
         pokemon1 = Blastoise
-    elif Selection == Venasaur:
+    elif Selection == 'Venasaur':
         pokemon1 = Venasaur
-    elif Selection == Pikachu:
+    elif Selection == 'Pikachu':
         pokemon1 = Pikachu
     else:
         print "Please type out the full name"
@@ -24,7 +26,7 @@ else:
 enemy = random.choice([Absol, Charizard, Blastoise, Venasaur, Pikachu])
 if enemy == pokemon1:
     enemy = random.choice([Absol, Charizard, Blastoise, Venasaur, Pikachu])
-
+print "Your Rival chose: %s" % (enemy)
 #names the user's pokemon
 
 
@@ -32,7 +34,7 @@ if enemy == pokemon1:
 for turn in range(1000):
     #setting up the parameters for the attacks
     if pokemon1 == Charizard:
-        cAttacks = raw_input.lower(('Flamethrower Ember Firekick Flame Tempest: '))
+        cAttacks = raw_input(('Flamethrower Ember Firekick Flame Tempest: '))
         flamethrower = random.randint(20,30)
         ember = random.randint(10,15)
         firekick = random.randint(15,20)
@@ -57,7 +59,7 @@ for turn in range(1000):
                 print "Please type the full attack"
     else:
         if pokemon1 == Blastoise:
-            bAttacks = raw_input.lower(('WaterCanon Squirt Tackle Hydropump: '))
+            bAttacks = raw_input(('WaterCanon Squirt Tackle Hydropump: '))
             watercanon = random.randint(20,25)
             squirt= random.randint(15,20)
             tackle = random.randint(15,20)
@@ -81,7 +83,7 @@ for turn in range(1000):
                 else:
                     print "Please type the full attack"            
         if pokemon1 == Venasaur:
-            vAttacks = raw_input.lower(('VineWhip RazorLeaf SolarBeam Cut: '))
+            vAttacks = raw_input(('VineWhip RazorLeaf SolarBeam Cut: '))
             vinewhip= random.randint(10,15)
             razorleaf = random.randint(15,25)
             solarbeam = random.randint(20,30)
@@ -105,7 +107,7 @@ for turn in range(1000):
                 else:
                     print "Please type the full attack"            
         if pokemon1 == Absol:
-            AbsolAttacks = raw_input.lower(('Bite HyperBeam Tackle Slash:  '))
+            AbsolAttacks = raw_input(('Bite HyperBeam Tackle Slash:  '))
             bite = random.randint(20,30)
             hyperbeam = random.randint(25,35)
             tackle = random.randint(10,15)
@@ -129,7 +131,7 @@ for turn in range(1000):
                 else:
                     print "Please type the full attack"            
         if pokemon1 == Pikachu:
-            PikachuAttack = raw_input.lower(('Slam Thunder Thunderbolt Tackle: '))
+            PikachuAttack = raw_input(('Slam Thunder Thunderbolt Tackle: '))
             slam = random.randint(10,20)
             thunder = random.randint(25,30)
             thunderbolt = random.randint(15,20)
@@ -152,129 +154,117 @@ for turn in range(1000):
                 
                 else:
                     print "Please type the full attack"            
-    
-    if enemy == Charizard:
-        enemyAttacks == random.choice([Flamethrower, Ember, Firekick, Flame Tempest])
-        Flamethrower = random.randint(20,30)
-        Ember = random.randint(10,15)
-        Firekick = random.randint(15,20)
-        Flametempest = random.randint(20,25)
-        if enemyAttacks == Flamethrower:
-            damage= pokemon1 - Flamethrower
-            print 'Charizard used Flamethrower, %s took %d damage' % (pokemon1)(damage)
-        else:
-            if enemyAttacks == Ember:
-                damage= pokemon1-Ember
-                print 'Charizard used Ember, %s took %d damage' % (pokemon1)(damage)
-               
-            elif enemyAttacks == Firekick:
-                damage = pokemon1-Firekick
-                print 'Charizard used Firekick, %s took %d damage' % (pokemon1)(damage)
-               
-            elif enemyAttacks == flametempest:
-                damage = pokemon1-flametempest
-                print 'Charizard used Flametempest, %s took %d damage' % (pokemon1)(damage)
-    
+
+if enemy == Charizard:
+    enemyAttacks == random.choice([Flamethrower, Ember, Firekick, Flametempest])
+    Flamethrower = random.randint(20,30)
+    Ember = random.randint(10,15)
+    Firekick = random.randint(15,20)
+    Flametempest = random.randint(20,25)
+    if enemyAttacks == Flamethrower:
+        damage= pokemon1 - Flamethrower
+        print 'Charizard used Flamethrower, %s took %d damage' % (pokemon1)(damage)
     else:
-        if enemy == Blastoise:
-            enemyAttacks = random.choice([watercanon, squirt, tackle, hydropump])
-            watercanon = random.randint(20,25)
-            squirt= random.randint(15,20)
-            tackle = random.randint(15,20)
-            hydropump = random.randint(25,35)
-            if enemyAttacks == watercanon:
-                damage= pokemon1 - watercanon
-                print 'Blastoise used Watercanon, %s took %d damage' % (pokemon1)(damage)
-            else:
-                if enemyAttacks == squirt:
-                    damage= pokemon1-squirt
-                    print 'Blastoise used Squirt, %s took %d damage' % (pokemon1)(damage)
-                
-                elif enemyAttacks == tackle:
-                    damage = pokemon1-tackle
-                    print 'Blastoise used Tackle, %s took %d damage' % (pokemon1)(damage)
-                
-                elif enemyAttacks == hydropump:
-                    damage = pokemon1-hydropump
-                    print 'Blastoise used hydropump, %s took %d damage' % (pokemon1)(damage)
+        if enemyAttacks == Ember:
+            damage= pokemon1-Ember
+            print 'Charizard used Ember, %s took %d damage' % (pokemon1)(damage)
            
-        if enemy == Venasaur:
-            enemyAttacks = random.choice.lower([vinewhip, razorleaf, solarbeam, cut])
-            vinewhip= random.randint(10,15)
-            razorleaf = random.randint(15,25)
-            solarbeam = random.randint(20,30)
-            cut = random.randint(10,15)
-            if enemyAttacks == vinewhip:
-                damage= pokemon1 - vinewhip
-                print 'Venasaur used VineWhip, %s took %d damage' % (pokemon1)(damage)
-            else:
-                if enemyAttacks == razorleaf:
-                    damage= pokemon1-razorleaf
-                    print 'Venasaur used RazorLeaf, %s took %d damage' % (pokemon1)(damage)
-                
-                elif enemyAttacks == solarbeam:
-                    damage = pokemon1-solarbeam
-                    print 'Venasaur used SolarBeam, %s took %d damage' % (pokemon1)(damage)
-                
-                elif enemyAttacks == cut:
-                    damage = pokemon1-cut
-                    print 'Venasaur used Cut, %s took %d damage' % (pokemon1)(damage)
+        elif enemyAttacks == Firekick:
+            damage = pokemon1-Firekick
+            print 'Charizard used Firekick, %s took %d damage' % (pokemon1)(damage)
+           
+        elif enemyAttacks == Flametempest:
+            damage = pokemon1-Flametempest
+            print 'Charizard used Flametempest, %s took %d damage' % (pokemon1)(damage)
+
+else:
+    if enemy == Blastoise:
+        enemyAttacks = random.choice([watercanon, squirt, tackle, hydropump])
+        watercanon = random.randint(20,25)
+        squirt= random.randint(15,20)
+        tackle = random.randint(15,20)
+        hydropump = random.randint(25,35)
+        if enemyAttacks == watercanon:
+            damage= pokemon1 - watercanon
+            print 'Blastoise used Watercanon, %s took %d damage' % (pokemon1)(damage)
+        else:
+            if enemyAttacks == squirt:
+                damage= pokemon1-squirt
+                print 'Blastoise used Squirt, %s took %d damage' % (pokemon1)(damage)
             
-        if enemy == Absol:
-            enemyAttacks = random.choice([bite, hyperbeam, tackle, slash])
-            bite = random.randint(20,30)
-            hyperbeam = random.randint(25,35)
-            tackle = random.randint(10,15)
-            slash = random.randint(15,20)
-            if enemyAttacks == bite:
-                damage= pokemon1 - bite
-                print 'Absol used Bite, %s took %d damage' % (pokemon1)(damage)
-            else:
-                if enemyAttacks == hyperbeam:
-                    damage= pokemon1-hyperbeam
-                    print 'Absol used HyperBeam, %s took %d damage' % (pokemon1)(damage)
-                
-                elif enemyAttacks == tackle:
-                    damage = pokemon1-tackle
-                    print 'Absol used Tackle, %s took %d damage' % (pokemon1)(damage)
-                
-                elif enemyAttacks == slash:
-                    damage = pokemon1-slash
-                    print 'Absol used Slash, %s took %d damage' % (pokemon1)(damage)
-                        
-        if enemy == Pikachu:
-            enemyAttacks = random.choice([slam, thunder, thunderbolt, tackle])
-            slam = random.randint(10,20)
-            thunder = random.randint(25,30)
-            thunderbolt = random.randint(15,20)
-            tackle = random.randint(10,15)
-            if enemyAttacks == slam:
-                damage= pokemon1 - slam
-                print 'Pikachu used Slam, %s took %d damage' % (pokemon1)(damage)
-            else:
-                if enemyAttacks == thunder:
-                    damage= pokemon1-thunder
-                    print 'Pikachu used Thunder, %s took %d damage' % (pokemon1)(damage)
-                
-                elif enemyAttacks == thunderbolt:
-                    damage = pokemon1-thunderbolt
-                    print 'Pikachu used Thunderbolt, %s took %d damage' % (pokemon1)(damage)
-                
-                elif enemyAttacks == tackle:
-                    damage = pokemon1-tackle
-                    print 'Pikachu used Tackle, %s took %d damage' % (pokemon1)(damage)
-
-
-
-
-
-    #win conditions
-    if pokemon1 <= 0:
-        print "Oh no! %s fainted!" % (pokemon1)
-        break
-    if enemy <= 0:
-        print "%s fainted! %s wins!" % (enemy)(pokemon1)
-        break
-    print "%s's health is " + str(pokemon1) %(pokemon1)
-    print "%s's health is " + str(enemy) %(enemy)
+            elif enemyAttacks == tackle:
+                damage = pokemon1-tackle
+                print 'Blastoise used Tackle, %s took %d damage' % (pokemon1)(damage)
+            
+            elif enemyAttacks == hydropump:
+                damage = pokemon1-hydropump
+                print 'Blastoise used hydropump, %s took %d damage' % (pokemon1)(damage)
+       
+    if enemy == Venasaur:
+        enemyAttacks = random.choice.lower([vinewhip, razorleaf, solarbeam, cut])
+        vinewhip= random.randint(10,15)
+        razorleaf = random.randint(15,25)
+        solarbeam = random.randint(20,30)
+        cut = random.randint(10,15)
+        if enemyAttacks == vinewhip:
+            damage= pokemon1 - vinewhip
+            print 'Venasaur used VineWhip, %s took %d damage' % (pokemon1)(damage)
+        else:
+            if enemyAttacks == razorleaf:
+                damage= pokemon1-razorleaf
+                print 'Venasaur used RazorLeaf, %s took %d damage' % (pokemon1)(damage)
+            
+            elif enemyAttacks == solarbeam:
+                damage = pokemon1-solarbeam
+                print 'Venasaur used SolarBeam, %s took %d damage' % (pokemon1)(damage)
+            
+            elif enemyAttacks == cut:
+                damage = pokemon1-cut
+                print 'Venasaur used Cut, %s took %d damage' % (pokemon1)(damage)
+        
+    if enemy == Absol:
+        enemyAttacks = random.choice([bite, hyperbeam, tackle, slash])
+        bite = random.randint(20,30)
+        hyperbeam = random.randint(25,35)
+        tackle = random.randint(10,15)
+        slash = random.randint(15,20)
+        if enemyAttacks == bite:
+            damage= pokemon1 - bite
+            print 'Absol used Bite, %s took %d damage' % (pokemon1)(damage)
+        else:
+            if enemyAttacks == hyperbeam:
+                damage= pokemon1-hyperbeam
+                print 'Absol used HyperBeam, %s took %d damage' % (pokemon1)(damage)
+            
+            elif enemyAttacks == tackle:
+                damage = pokemon1-tackle
+                print 'Absol used Tackle, %s took %d damage' % (pokemon1)(damage)
+            
+            elif enemyAttacks == slash:
+                damage = pokemon1-slash
+                print 'Absol used Slash, %s took %d damage' % (pokemon1)(damage)
+                    
+    if enemy == Pikachu:
+        enemyAttacks = random.choice([slam, thunder, thunderbolt, tackle])
+        slam = random.randint(10,20)
+        thunder = random.randint(25,30)
+        thunderbolt = random.randint(15,20)
+        tackle = random.randint(10,15)
+        if enemyAttacks == slam:
+            damage= pokemon1 - slam
+            print 'Pikachu used Slam, %s took %d damage' % (pokemon1)(damage)
+        else:
+            if enemyAttacks == thunder:
+                damage= pokemon1-thunder
+                print 'Pikachu used Thunder, %s took %d damage' % (pokemon1)(damage)
+            
+            elif enemyAttacks == thunderbolt:
+                damage = pokemon1-thunderbolt
+                print 'Pikachu used Thunderbolt, %s took %d damage' % (pokemon1)(damage)
+            
+            elif enemyAttacks == tackle:
+                damage = pokemon1-tackle
+                print 'Pikachu used Tackle, %s took %d damage' % (pokemon1)(damage)
     
+
+                
